058d79bc84e04c5fb9aa0614d2ed2247094875fd
Shachar Itzhaky <corwin.amber@gmail.com>
2020-03-23 17:25:15 +0200

    [refactor] Share core functionality between worker and cli.

---
+++ src/backend/core.ts
@@ -0,0 +1,197 @@
+import { EventEmitter } from 'events';
+import JSZip from 'jszip';
+import { DEFLATE } from 'jszip/lib/compressions'
+import { inflateRaw } from 'pako';
+
+import { OCamlExecutable } from './ocaml_exec';
+
+
+
+class IcoqPod extends EventEmitter {
+
+    core: OCamlExecutable
+
+    binDir: string
+    io: IO
+
+    constructor(binDir?: string) {
+        super();
+        binDir = binDir || (typeof fetch === 'undefined'
+                            || process.env.NODE_NOW ? './bin' : '../bin');
+        this.binDir = binDir;
+
+        this.core = new OCamlExecutable({stdin: false, tty: false, binDir});
+        this.core.debug = () => {};
+        this.core.trace = () => {};        
+
+        var utf8 = new TextDecoder();
+        this.core.on('stream:out', ev => console.log(utf8.decode(ev.data)));
+
+        this.io = new IO;
+    }
+
+    get fs() { return this.core.wasmFs.fs; }
+
+    async boot() {
+        await this.upload(`${this.binDir}/icoq.bc`, '/lib/icoq.bc');
+    
+        this._preloadStub();
+    
+        await this.core.run('/lib/icoq.bc', [], ['wacoq_post']);
+    
+        await this.loadPackage('+init', false);    
+    }
+
+    async upload(fromUri: string, toPath: string) {
+        var content = await this.io._fetch(fromUri);
+        this.putFile(toPath, content);
+    }
+
+    loadPackage(uri: string, refresh: boolean = true) {
+        return this.loadPackages([uri], refresh);
+    }
+
+    async loadPackages(uris: string | string[], refresh: boolean = true) {
+        if (typeof uris == 'string') uris = [uris];
+        
+        await Promise.all(uris.map(async uri => {
+            await this.unzip(uri, '/lib');
+            this._progress(uri, undefined, true);
+        }));
+
+        if (refresh)
+            this.command(['RefreshLoadPath']);
+    }
+
+    async loadSources(uri: string, dirpath: string) {
+        var subdir = dirpath.replace(/[.]|(?<=[^/])$/g, '/');
+        this.unzip(uri, `/src/${subdir}`);
+    }
+
+    unzip(uri: string, dir: string) {
+        return this.io.unzip(this._pkgUri(uri),
+                    (fn, ui8a) => this.putFile(`${dir}/${fn}`, ui8a),
+                    p => this._progress(uri, p));
+    }
+
+    _pkgUri(uri: string) {
+        return (uri[0] == '+') ?
+            `${this.binDir}/coq/${uri.substring(1)}.coq-pkg` : uri;
+    }
+
+    _progress(uri: string, download: DownloadProgress, done = false) {
+        this.emit('progress', {uri, download, done});
+    }
+
+    putFile(filename: string, content: Uint8Array | string) {
+        // needs to be synchronous
+        this.fs.mkdirpSync(filename.replace(/[/][^/]+$/, ''))
+        this.fs.writeFileSync(filename, content);
+    }
+
+    command(cmd: any[]) {
+        switch (cmd[0]) {
+        case 'LoadPkg':   this.loadPackages(cmd[1]);               return;
+        case 'Put':       this.putFile(cmd[1], cmd[2]);            return;
+        }
+
+        const wacoq_post = this.core.callbacks && this.core.callbacks.wacoq_post;
+        if (!wacoq_post) return;
+
+        var json = (typeof cmd === 'string') ? cmd : JSON.stringify(cmd),
+            answer = wacoq_post(this.core.to_caml_string(json));
+        this._answer(answer);
+    }
+    
+    answer(msgs: any[][]) {
+        for (let msg of msgs) this.emit('message', msg);
+    }
+    
+    _answer(ptr: number) {
+        var cstr = this.core.proc.userGetCString(ptr);
+        this.answer(JSON.parse(<any>cstr));
+    }
+
+    /**
+     * (internal) Initializes the dllbyterun_stub shared library.
+     */
+    _preloadStub() {
+        this.core.proc.dyld.preload(
+            'dllbyterun_stubs.so', `${this.binDir}/coq/dllbyterun_stubs.wasm`,
+            {
+                data: ['caml_atom_table'], func: ['caml_copy_double'],
+                js: {
+                    wacoq_emit_js: (s:number) => this._answer(s)
+                }
+            }
+        );
+    }    
+}
+
+
+class IO {
+
+    async unzip(zip: string | JSZip, put: (filename: string, content: Uint8Array) => void, progress?: (p: any) => void) {
+        if (typeof zip == 'string')
+            zip = await JSZip.loadAsync(await this._fetch(zip, progress));
+
+        let yc = 0;
+        for (let entry of zip.filter((_, e) => !e.dir)) {
+            put(entry.name, this._inflateFast(entry));
+            if (!((++yc) & 0xf)) await _yield();
+        }
+    }
+
+    _inflateFast(entry: any) {
+        if (entry._data.compression == DEFLATE)
+            return inflateRaw(entry._data.compressedContent);
+        else /* STORE */
+            return entry._data.compressedContent;
+    }
+
+    async _fetch(uri: string, progress?: (p: any) => void) : Promise<Uint8Array> {
+        if (progress && typeof fetch !== 'undefined') {
+            return this._toU8A(this._fetchWithProgress(uri, progress));
+        }
+        else return this._fetchSimple(uri);
+    }
+
+    async _toU8A(blob: Promise<Blob>) {
+        return new Uint8Array(await (await blob).arrayBuffer());
+    }
+
+    async _fetchSimple(uri: string) {
+        if (typeof fetch !== 'undefined') {
+            return new Uint8Array(await (await fetch(uri)).arrayBuffer())
+        }
+        else {
+            const fs = require('fs');
+            return (0||fs.readFileSync)(uri);
+        }
+    }
+
+    // boilerplate
+    async _fetchWithProgress(uri: string, progress: (p: any) => void) {
+        var response = await fetch(uri),
+            total = +response.headers.get('Content-Length'),
+            r = response.body.getReader(), chunks = [], downloaded = 0;
+        for(;;) {
+            var {value, done} = await r.read();
+            if (done) break;
+            chunks.push(value);
+            downloaded += value.length;
+            progress({total, downloaded})
+        }
+        return new Blob(chunks);
+    }
+
+}
+
+function _yield() { return new Promise(resolve => setTimeout(resolve, 0)); }
+
+
+type DownloadProgress = { total: number, downloaded: number };
+
+
+
+export  { IcoqPod, DownloadProgress }
--- src/cli.ts
+++ src/cli.ts
@@ -1,50 +1,26 @@
 // Build with
 //  parcel watch --target node src/cli.ts
 
-import * as fs from 'fs';
-
-import { PackageManager, Resource } from 'basin-shell';
-import { OCamlExecutable } from './backend/ocaml_exec';
 import { FormatPrettyPrint } from './ui/format-pprint';
+import { IcoqPod } from './backend/core';
 
 
-
-var core: OCamlExecutable, pm: PackageManager;
-
-
-class FileResource extends Resource {
-    blob() {
-        return <any>fs.readFileSync(this.uri); // sorry
-    }
-}
 
 async function main() {
-    var binDir = 'bin';
 
-    core = new OCamlExecutable({stdin: false, tty: false, binDir});
-    core.debug = () => {};
-    core.trace = () => {};
+    var icoq = new IcoqPod();
+    icoq.on('message', handleIncoming);
 
-    var utf8 = new TextDecoder();
+    await icoq.boot();
 
-    core.on('stream:out', ev => console.log(utf8.decode(ev.data)));
+    icoq.command(['Init', {top_name: 'LF.Basics'}]);
+    icoq.command(['Add', null, 2, 'Check 0.', true]);
+    icoq.command(['Exec', 2]);
 
-    async function copy(fromPath, toPath) {
-        var content = fs.readFileSync(fromPath);
-        core.wasmFs.fs.writeFileSync(toPath, new Uint8Array(content));
-    }
+    return;
+}
+/*
 
-    pm = new PackageManager(core.wasmFs.volume);
-    await pm.install({
-        "/lib/": new FileResource(`${binDir}/coq/coq-all.coq-pkg`),
-    }, false);
-
-    core.wasmFs.fs.mkdirpSync('/lib');
-    await copy(`${binDir}/icoq.bc`, '/lib/icoq.bc');
-
-    preloadStub(core);
-
-    core.wasmFs.fs.mkdirpSync('/lib/LF');
     copy('examples/lf/Basics.v', '/lib/LF/Basics.v');
     copy('examples/lf/Induction.v', '/lib/LF/Induction.v');
 
@@ -64,42 +40,14 @@ async function main() {
 
     //console.log(core.wasmFs.fs.readFileSync('/home/Module.vo'));
 }
+*/
 
+const pp = () => new FormatPrettyPrint();
 
-function handleOutgoing(cmd: any[]) {
-    const callbacks = core.callbacks;
-
-    if (!callbacks.wacoq_post) return;
-
-    var json = (typeof cmd === 'string') ? cmd : JSON.stringify(cmd),
-        answer = callbacks.wacoq_post(core.to_caml_string(json));
-    _handleIncoming(answer);
-}
-
-function handleIncoming(msgs: any[][]) {
-    for (let msg of msgs) {
-        if (msg[0] != 'Feedback') console.log(msg);
-        if (msg[0] == 'Feedback' && msg[1].contents[0] == 'Message')
-            console.log(new FormatPrettyPrint().pp2Text(msg[1].contents[3]));
-    }
-}
-
-function _handleIncoming(ptr: number) {
-    var cstr = core.proc.userGetCString(ptr);
-    handleIncoming(JSON.parse(cstr.toString('utf-8')));
-}
-
-
-function preloadStub(core: OCamlExecutable) {
-    core.proc.dyld.preload(
-        'dllbyterun_stubs.so', `${core.opts.binDir}/coq/dllbyterun_stubs.wasm`,
-        {
-            data: ['caml_atom_table'], func: ['caml_copy_double'],
-            js: {
-                wacoq_emit_js: _handleIncoming
-            }
-        }
-    );
+function handleIncoming(msg: any[]) {
+    if (msg[0] != 'Feedback') console.log(msg);
+    if (msg[0] == 'Feedback' && msg[1].contents[0] == 'Message')
+        console.log(pp().pp2Text(msg[1].contents[3]));
 }
 
 
